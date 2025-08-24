6a52d756e325712e61febe77139a796952627589
qkrwlgh91 <qkrwlgh91@gmail.com>
2025-08-24 23:21:16 +0900

    validating input on every keystroke via state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted!");
@@ -44,6 +47,9 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -52,7 +58,9 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            onChange={(event) => handleInputChange('password', event.target.value)}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
             value={enteredValues.password}
           />
         </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,7 +9,7 @@ export default function Signup() {
 
     console.log(data);
 
-    event.target.reset();
+    // event.target.reset();
   }
   return (
     <form onSubmit={handleSubmit}>
