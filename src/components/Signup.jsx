437e143c75ddaf66d969c1a7819cf453aed3cd83
qkrwlgh91 <qkrwlgh91@gmail.com>
2025-08-24 22:28:25 +0900

    form reset

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -8,6 +8,8 @@ export default function Signup() {
     data.acquisition = acquisitionChannel;
 
     console.log(data);
+
+    event.target.reset();
   }
   return (
     <form onSubmit={handleSubmit}>
